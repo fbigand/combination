cdef9b7444477d12e616b46f75d92f86ca73e38b
fbigand <fbigand@etu.imt-lille-douai.fr>
2021-04-27 22:00:33 +0200

    remove unused function from slice.go

--- util/slice.go
+++ util/slice.go
@@ -7,24 +7,15 @@ func InitSliceInt(slice []int, defaultValue int) {
 	}
 }
 
-// InitSliceUint set defaultValue to all element of uint slice
-func InitSliceUint(slice []uint, defaultValue uint) {
-	for i := range slice {
-		slice[i] = defaultValue
-	}
-}
-
 // Compare two slice of uint and returns true if they
 // contains same values in same order
 func EqualsSliceUint(s1, s2 []uint) bool {
 	if len(s1) != len(s2) {
 		return false
 	}
-	for s1Index := range s1 {
-		for s2Index := range s2 {
-			if s1[s1Index] != s2[s2Index] {
-				return false
-			}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
 		}
 	}
 	return true
